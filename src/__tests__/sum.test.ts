12f2175b05470c35962e02f683ce5156244192e7
megatheikal <64621442+megatheikal@users.noreply.github.com>
2023-04-19 00:09:03 +0800

    remove boilerplate unit test case

--- src/__tests__/sum.test.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "../sum";
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(api(1, 2)).toBe(3);
-});
--- src/sum.ts
+++
@@ -1,3 +0,0 @@
-export function api(a:number, b:number) {
-    return a + b;
-  }
